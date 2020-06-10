3e1878449307db2a9c70d80c90ca89449b4a6a4b
kimondo88 <kimondo1988@o2.pl>
2020-06-10 20:24:38 +0200

    finished game lobby commit

--- game_lobby/game_lobby.cpp
+++ game_lobby/game_lobby.cpp
@@ -66,8 +66,9 @@ Lobby::~Lobby()
 void Lobby::AddPlayer()
 {
     cout << "Please enter the name of the new player: ";
+    
     string name;
-    getline(cin, name);
+    cin >> name;
     Player* pNewPlayer = new Player(name);
     if(m_pHead == 0) // if list is empty make head of list this new player
     {
@@ -96,4 +97,69 @@ void Lobby::RemovePlayer()
         m_pHead = m_pHead -> GetNext();
         delete pTemp;
     }
+}
+
+void Lobby::Clear()
+{
+    while(m_pHead != 0)
+    {
+        RemovePlayer();
+    }
+}
+
+ostream& operator<<(ostream& os, const Lobby& aLobby)
+{
+    Player* pIter = aLobby.m_pHead;
+    os << "\nHere is who is in the lobby:\n";
+    if(pIter == 0)
+    {
+        os << "Lobby is empty.\n";
+    }
+    else
+    {
+        while(pIter != 0)
+        {
+            os << pIter->GetName() << endl;
+            pIter = pIter->GetNext();
+        }
+    }
+    return os;
+}
+
+int main()
+{
+    Lobby myLobby;
+    int choice;
+    do
+    {
+        cout << myLobby;
+
+        cout << "\nGAME LOBBY\n";
+        cout << "0 - Exit the program.\n";
+        cout << "1 - Add a player to the lobby.\n";
+        cout << "2 - Remove player from the lobby.\n";
+        cout << "3 - Clear the lobby.\n";
+
+        cout << endl << "Enter choice: ";
+        cin >> choice;
+        switch(choice) 
+        {
+            case 0:
+                cout << "Good-bye.\n";
+                break;
+            case 1:
+                myLobby.AddPlayer();
+                break;
+            case 2:
+                myLobby.RemovePlayer();
+                break;
+            case 3:
+                myLobby.Clear();
+                break;
+            default:
+                cout << "That was not a valid choice.\n";
+        }
+    } while (choice != 0);
+
+    return 0;
 }
