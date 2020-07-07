20fb80a34af868044cf0550d227bc28bd19a5bcb
kimondo88 <kimondo1988@o2.pl>
2020-07-07 19:21:38 +0200

    blackjack done, minor commit

--- blackjack/blackjack.cpp
+++ blackjack/blackjack.cpp
@@ -449,4 +449,27 @@ ostream& operator<<(ostream& os, const Card& aCard)
     return os;
 }
 
-ostream
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer)
+{
+    os << aGenericPlayer.m_Name << ":\t";
+    vector<Card*>::const_iterator pCard;
+    if(!aGenericPlayer.m_Cards.empty())
+    {
+        for(pCard = aGenericPlayer.m_Cards.begin(); 
+        pCard != aGenericPlayer.m_Cards.end();
+        ++pCard)
+        {
+            os << *(*pCard) << "\t";
+        }
+        if(aGenericPlayer.GetTotal() !=0)
+        {
+            cout << "(" << aGenericPlayer.GetTotal() << ")";
+        }
+    }
+    else
+    {
+        os << "<empty>";
+    }
+
+    return os;
+}
